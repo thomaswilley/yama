32698cb10ab3b7e3c04ec1b16e67853534d77768
tlw <thomas.willey@emc.com>
2014-04-27 15:59:56 -0400

    the basics working

--- README.md
+++ README.md
@@ -4,8 +4,29 @@ yama
 
 yet another mail app
 
-work in progres, not ready for use
+work in progres
 
 syncs a local db (probably couchdb) instance with an email inbox (imap)
 i.e., enables a local RESTful interface to plain remote email with
 a powerful database to use for fun things later.
+
+---
+install:
+---
+
+```bash
+virtualenv --no-site-packages --distribute -pPython2.7 venv
+source venv/bin/activate
+pip install git+https://github.com/balsagoth/imbox
+pip install -r requirements.txt
+```
+
+---
+usage:
+---
+```bash
+couchdb
+source venv/bin/activate
+python yama.py -imap "imap.gmail.com" -u "you@gmail.com" -p
+"YOUR_GMAIL_PW" -db "http://127.0.0.1:5984/"
+```
--- yama.py
+++ yama.py
@@ -1,13 +1,10 @@
+import argparse
 from imbox import Imbox
 from getpass import getpass
 import couchdb
 import sha
 import json
 import pdb
-
-imap_server = raw_input('imap url: ')
-username = raw_input('username: ')
-password = getpass('password [%s]: ' % username)
 
 def default(obj):
     """Default JSON serializer."""
@@ -17,40 +14,92 @@ def default(obj):
         if obj.utcoffset() is not None:
             obj = obj - obj.utcoffset()
         millis = int(
-            calendar.timegm(obj.timetuple()) * 1000 +
-            obj.microsecond / 1000
-        )
+                calendar.timegm(obj.timetuple()) * 1000 +
+                obj.microsecond / 1000
+                )
         return millis
+
     if isinstance(obj, set):
         return list(obj)
 
-    #pdb.set_trace()
-    return obj.__dict__
+    return obj
 
 def sync_message_with_couchdb(message_uid, message, db):
-    json_message = json.dumps(message, default=default)
+    # hackishly try to seralize across a couple unicode encodings
+    try:
+        json_message = json.dumps(message.__dict__, default=default, encoding="utf-8")
+    except:
+        try:
+            json_message = json.dumps(message.__dict__, default=default, encoding="ISO-8859-1")
+        except:
+            pass
+    if not json_message:
+        print "failed to seralize message %s" % message_uid
+        return None
 
-    #pdb.set_trace()
-    return db.save(json_message, _id=message_uid)
+    dict_message = json.loads(json_message)
+    dict_message['_id'] = message_uid
 
-# https://pythonhosted.org/CouchDB/getting-started.html
-# assumes couchdb is running prior to running this...
-couch = couchdb.Server('http://127.0.0.1:5984/')
-db = 'messages_%s' % sha.sha(username).hexdigest()
-couchdb = None
-try:
-    couchdb = couch[db]
-except:
-    couchdb = couch.create(db) # db didn't exist so create
+    try:
+        id, rev = db.save(dict_message, _id=message_uid)
+    except couchdb.http.ResourceConflict:
+        print "message %s already exists, skipping" % message_uid
+        return None
 
-# https://github.com/martinrusev/imbox
-# note: using fresher fork: https://github.com/balsagoth/imbox
-imbox = Imbox(imap_server, username=username, password=password, ssl='SSL')
+    return (id, rev)
 
-# todo: sync point (set from_date to) day prior to most recent day stored in db
-from_date = '14-Apr-2013'
-all_messages = imbox.messages(date__gt=from_date)
+def sync(imap_endpoint, couchdb_endpoint, username, password):
+    # https://pythonhosted.org/CouchDB/getting-started.html
+    # assumes couchdb is running prior to running this...
+    couch = couchdb.Server(couchdb_endpoint)
+    dbname = 'messages_%s' % sha.sha(username).hexdigest()
+    db = None
+    try:
+        db = couch[dbname]
+    except:
+        db = couch.create(dbname) # db didn't exist so create
 
-print "syncing messages to couch db [%s]" % db
-for uid, message in all_messages:
-    print uid, message.subject, sync_message_with_couchdb(uid, message, couchdb)
+    # https://github.com/martinrusev/imbox
+    # note: using fresher fork: https://github.com/balsagoth/imbox
+    imbox = Imbox(imap_endpoint, username=username, password=password, ssl='SSL')
+
+    # todo: sync point (set from_date to) day prior to most recent day stored in db
+    from_date = '14-Apr-2013'
+    all_messages = imbox.messages(date__gt=from_date)
+
+    print "syncing messages to couch db [%s]" % db
+
+    for uid, message in all_messages:
+        print uid, message.subject, sync_message_with_couchdb(uid, message, db)
+
+def deletedb(couchdb_endpoint, username):
+    couch = couchdb.Server(couchdb_endpoint)
+    dbname = 'messages_%s' % sha.sha(username).hexdigest()
+    print couch.delete(dbname) # db didn't exist so create
+
+def main():
+    parser = argparse.ArgumentParser(
+            description='sync mail account to couchdb'
+            )
+    parser.add_argument('-imap', metavar="imap_endpoint",
+            type=str, help='url to imap endpoint', required=True)
+    parser.add_argument('-u', metavar="imap_username",
+            type=str, help='imap username (typically email address)', required=True)
+    parser.add_argument('-p', metavar="imap_password",
+            type=str, help='imap password for username', required=True)
+    parser.add_argument('-db', metavar="couchdb_endpoint",
+            type=str, help='url to couchdb endpoint', required=True)
+    parser.add_argument('-deletedb',
+            action='store_true',
+            default=False,
+            help='will DELETE the local couchdb database associated with this account')
+
+    args = parser.parse_args()
+
+    if args.deletedb:
+        deletedb(args.db, args.u)
+    else:
+        sync(args.imap, args.db, args.u, args.p)
+
+if __name__ == "__main__":
+    main()
